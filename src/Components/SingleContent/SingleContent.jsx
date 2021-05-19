258969916a0691d5240312a89f4f8670ed97edca
Saurav Lal <47920659+saurav-skl@users.noreply.github.com>
2021-05-19 15:22:45 +0530

    file formating and designing using framer (#1)

--- src/Components/SingleContent/SingleContent.jsx
+++ src/Components/SingleContent/SingleContent.jsx
@@ -1,8 +1,9 @@
-import "./SingleContent.css";
 import { Badge } from "@material-ui/core";
 import { img_300, unavailable } from "../../config/config";
 import React from "react";
-import ContentModal from "../Modal/ContentModal"
+import ContentModal from "../Modal/ContentModal";
+import { motion } from "framer-motion";
+import "./SingleContent.css";
 
 const SingleContent = ({
   id,
@@ -14,22 +15,30 @@ const SingleContent = ({
 }) => {
   return (
     <>
-      <ContentModal media_type={media_type} id={id}>
-        <Badge
-          badgeContent={vote_average}
-          color={vote_average > 6 ? "primary" : "secondary"}
-        />
-        <img
-          className="poster"
-          src={poster ? `${img_300}${poster}` : unavailable}
-          alt={title}
-        />
-        <b className="title">{title}</b>
-        <span className="subTitle">
-          {media_type === "tv" ? "TV Series" : "Movie"}
-          <span className="subTitle">{date}</span>
-        </span>
-      </ContentModal>
+      <motion.div
+        initial={{ x: -200, y: 200, opacity: 0 }}
+        animate={{ x: 0, y: 0, opacity: 1 }}
+        transition={{ duration: 0.8 }}
+        whileHover={{ scale: 1.07 }}
+        whileTap={{ scale: 0.9 }}
+      >
+        <ContentModal media_type={media_type} id={id}>
+          <Badge
+            badgeContent={vote_average}
+            color={vote_average > 6 ? "primary" : "secondary"}
+          />
+          <img
+            className="poster"
+            src={poster ? `${img_300}${poster}` : unavailable}
+            alt={title}
+          />
+          <b className="title">{title}</b>
+          <span className="subTitle">
+            {media_type === "tv" ? "TV Series" : "Movie"}
+            <span className="subTitle">{date}</span>
+          </span>
+        </ContentModal>
+      </motion.div>
     </>
   );
 };
